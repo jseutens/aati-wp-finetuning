39e7558dd23bb13fa9f4bc28ce51f9e844d114da
jseutens <johan@seutens.eu>
2020-12-19 23:08:29 +0100

    Update aati-wp-finetuning.php

--- aati-wp-finetuning.php
+++ aati-wp-finetuning.php
@@ -9,3 +9,10 @@ Author URI: https://www.aati.be
 License: GPLv2 or later
 Text Domain: aatiwpfinetuning
  */
+
+// REMOVE WP EMOJI
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
