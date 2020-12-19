a9952e3ecc244822dc4263f5933c37e69e48c3a1
jseutens <johan@seutens.eu>
2020-12-19 22:51:34 +0100

    Create aati-wp-finetuning.php

---
+++ aati-wp-finetuning.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: AATI WP Finetuning
+Plugin URI: https://github.com/jseutens/aati-wp-finetuning/
+Description: 
+Version: 1.0
+Author: Johan Seutens
+Author URI: https://www.aati.be
+License: GPLv2
+ */
